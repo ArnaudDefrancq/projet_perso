c61d3659ae9cacf7d4ca60de275358a7240b3b8b
unknown <arnaud.defrancq@laposte.net>
2023-06-09 13:00:33 +0200

    Ajout des dépendances

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
+
+connectDB();
 
 const app = express();
 app.get("/", (req, res) => {
